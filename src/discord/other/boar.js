5ff0adbe7d1aa8daca150e014b1cf0a1ed0d711b
Zickles <76439587+Zickles@users.noreply.github.com>
2025-01-03 20:41:59 -0500

    fix(boar-ping): add correct code (#6)

--- src/discord/other/boar.js
+++ src/discord/other/boar.js
@@ -1,27 +1,18 @@
-const HypixelDiscordChatBridgeError = require("../../contracts/errorHandler.js");
-const { SuccessEmbed } = require("../../contracts/embedHandler.js");
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync } = require("fs");
+const cron = require("node-cron");
 
-module.exports = {
-  name: "boar-ping-leave",
-  description: "Stop getting pinged with boars",
+function boarPing() {
+  const boarData = readFileSync("data/boar.json");
+  if (!boarData) return;
+  const boar = JSON.parse(boarData);
+  if (!boar) return;
+  let message = "";
 
-  execute: async (interaction) => {
-    const boarData = readFileSync("data/boar.json");
-    if (!boarData) {
-      throw new HypixelDiscordChatBridgeError("The boar data file does not exist. Please contact an administrator.");
-    }
+  boar.forEach((user) => (message += `<@${user}> `));
 
-    let boar = JSON.parse(boarData);
-    if (!boar) {
-      throw new HypixelDiscordChatBridgeError("The boar data file is malformed. Please contact an administrator.");
-    }
-    boar = boar.filter((user) => user !== interaction.user.id);
-    writeFileSync("data/boar.json", JSON.stringify(boar, null, 2));
-    const embed = new SuccessEmbed("You will no longer be pinged for boar", {
-      text: `by @.kathund | /help [command] for more information`,
-      iconURL: "https://i.imgur.com/uUuZx2E.png",
-    });
-    await interaction.followUp({ embeds: [embed] });
-  },
-};
+  client.channels.fetch("1296611611393527864").then((channel) => {
+    if (channel) channel.send(message);
+  });
+}
+
+cron.schedule(`0 0 * * *`, () => boarPing(), { scheduled: true, timezone: "UTC" });
